91f49b1506b397b3f6b9059dae6dbc3e382995b9
PikaKight <tmmarcus73@gmail.com>
2021-06-15 22:18:32 -0400

    Added a log to keep track of what is being said, says hi to the user and a flow chart

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_mesg = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_mesg} in {channel}')
+
     if message.author == client.user:
         return 
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith('$off'):
-        await client.logout()
-        quit()
+    if message.channel.name == 'test':
+        if message.content.startswith('$hello'):
+            await message.channel.send(f'Hello {username}!')
+        
+        
+        elif message.content.startswith('$bye'):
+            await client.logout()
+            quit()
 
 with open('config.json', 'r') as f:
     config = json.load(f)
